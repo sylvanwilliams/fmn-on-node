c1258ac80cd580a2f8a0b456977c7b7c3911a02e
Sylvan Williams <sylvan.williams@gmail.com>
2014-09-05 08:27:44 -0700

    Styled map with old timey style from snazzy maps.com

--- public/javascripts/initializeMap.js
+++ public/javascripts/initializeMap.js
@@ -1,10 +1,108 @@
+var style_array = [
+    {
+        "featureType": "administrative",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#84afa3"
+            },
+            {
+                "lightness": 52
+            }
+        ]
+    },
+    {
+        "stylers": [
+            {
+                "saturation": -77
+            }
+        ]
+    },
+    {
+        "featureType": "road"
+    }
+];
+
 function initializeMap() {
 
 	        var myOptions = {
 	          center: new google.maps.LatLng(-34.397, 150.644),
 	          zoom: 16,
-	          mapTypeId: google.maps.MapTypeId.ROADMAP
-	        };
+	          mapTypeId: google.maps.MapTypeId.ROADMAP,
+            styles: style_array}
+
 	        var map = new google.maps.Map(document.getElementById("map_canvas"),
 	            myOptions);
 
