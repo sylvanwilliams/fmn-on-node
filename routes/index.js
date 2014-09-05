c892755aec0fe5311dfe8b16ca865a723e1105af
Sylvan Williams <sylvan.williams@gmail.com>
2014-09-04 18:00:24 -0700

    Added routes and skeleton view for other pages.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Forget Me Not' });
 });
 
+router.get('/location',function(req,res){
+  res.render('location',{title: 'Location' });
+});
+
+router.get('/goods',function(req,res){
+  res.render('goods',{title: 'Location' });
+});
+
+router.get('/treats',function(req,res){
+  res.render('treats',{title: 'Location' });
+});
+
 module.exports = router;
